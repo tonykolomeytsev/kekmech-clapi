213e373aa75a19690b14e522d54287373874f651
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2019-05-25 18:58:32 +0300

    Теперь во время установки соединения HANDSHAKE может быть обработан автоматически библиотекой, если указать device_id в конструкторе Clapi

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,12 @@
 #include <Clapi.h>
 
 Clapi *api = new Clapi(USBTX, USBRX);
+DigitalOut led1(PF_12);
+DigitalOut led2(PD_15);
+DigitalOut led3(PD_14);
+DigitalOut led4(PA_7);
+DigitalOut led5(PF_13);
+DigitalOut *leds = new DigitalOut[5]{led1, led2, led3, led4, led5};
 
 #define CMD_SUM 1
 #define CMD_TEST 2
@@ -19,6 +25,10 @@ void listener(int code, int argsCount, float args[]) {
             ->query("float", 3.51f)
             ->query("bool", true)
             ->send();
+    }
+    if (code == 3) {
+        int lednum = static_cast<int>(args[0]);
+        leds[lednum] = !leds[lednum];
     }
 }
 
